4f4e73da5022b7202fe35c62700c8437a25dd532
samiranghosh04 <62144424+samiranghosh04@users.noreply.github.com>
2023-08-20 23:18:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Form from './components/Form'
+
+function App() {
+  return (
+    <>
+      <div className="">
+        <Form />
+      </div>
+    </>
+  )
+}
+
+export default App
